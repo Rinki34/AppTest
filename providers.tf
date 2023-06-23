ee9ab9c469087d7c49a66514337c29baeb6945e7
Rinki34 <137409846+Rinki34@users.noreply.github.com>
2023-06-23 18:38:39 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-    version = ">=1.32"
+    version = "~>2.0"
     subscription_id = var.az_subscription_id
     tenant_id = var.az_tenant_id
     client_id = var.az_service_principal_client_id
