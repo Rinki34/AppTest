42618795d581baf3ba9eaa920e82cc0567bfe6cc
Rinki34 <137409846+Rinki34@users.noreply.github.com>
2023-06-23 20:18:43 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,7 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.66.0"
-      features {}
-    }
-  }
-
-  required_version = ">= 0.14"
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
 }
+ 
